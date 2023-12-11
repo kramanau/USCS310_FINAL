6590bd201691e4a382f0104036b49c5500263f7e
kramanau <kramanauskas112@gmail.com>
2023-12-11 00:18:41 -0500

    added timer

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let mouseDown = false;
 let currentStroke = 0;
 const canvas = $('#canvas');
-
+$("#start").toggleClass("hidden");
 
 const offsetX = 0;
 const offsetY = 75; //height of painttools bar
@@ -24,12 +24,32 @@ function validateRGB(){
     const R = $('#rInput').val();
     const G = $('#gInput').val();
     const B = $('#bInput').val();
-    pattern = 
-    userBrush.updateColor(R,G,B);
+    const pattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    if(pattern.test(R) && pattern.test(G) && pattern.test(B)){
+        userBrush.updateColor(R,G,B);
+    } else {
+        alert('Failed to set color.');
+    }
+}
+
+function startTimer(){
+    $("#start").toggleClass("hidden");
+    $("#timer").toggleClass("hidden");
+    const interval = setInterval(()=>{
+        const seconds = parseInt($("#timer").text().split(':')[1]);
+        const newSeconds = seconds - 1;
+        console.log(String(newSeconds).padStart(2,'0'));
+        $("#timer").text(':'+String(newSeconds).padStart(2,'0'));
+        if (newSeconds === 0){
+            clearInterval(interval);
+        }
+    }, 1000);
 }
 
 $(document).ready(function(){
     $("#setcolor").on('click', validateRGB);
+
+    $("#start").on('click', startTimer);
     
     $('#canvas').mousedown(function(e){
         currentStroke += 1;
